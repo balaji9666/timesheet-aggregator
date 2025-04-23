ea40e6f2abab546148daf69dc3c837c6acdb3e98
Balaji MS <58804855+balaji9666@users.noreply.github.com>
2025-04-23 12:56:38 +0530

    Https Enviroment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://16.170.220.172:3000/api',
+  apiUrl: 'https://16.170.220.172:3000/api',
   keyEncryptDecrypt: '5c88acf79eecbc7841@ar$tyudchtd^h',
   googleCredentialsPath: 'path/to/your/credentials.json',
   googleApiKey: 'YOUR_GOOGLE_API_KEY'
